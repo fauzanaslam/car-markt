b6a4d8d20240617b7b5d0bb075293556323438af
fauzanaslam <fauzanaslamdhani@gmail.com>
2025-05-06 21:32:22 +0700

    change api

--- apps/car-markt-web/src/app/environments/environment.prod.ts
+++ apps/car-markt-web/src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api: 'http://82.112.240.66:3000',
+  api: '/api/graphql',
 };
--- apps/car-markt-web/src/app/environments/environment.ts
+++ apps/car-markt-web/src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  api: 'http://82.112.240.66:3000',
+  api: '/api/graphql',
 };
