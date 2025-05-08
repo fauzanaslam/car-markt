36bae7029796a2ece613db8a482c648d56f08767
fauzanaslam <fauzanaslamdhani@gmail.com>
2025-05-08 10:02:44 +0700

    add image

---
+++ apps/car-markt-web/src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '@prisma/client';
+@Component({
+  selector: 'app-product-card',
+  imports: [CommonModule],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.css',
+})
+export class ProductCardComponent {
+  product = input.required<Product>();
+}
