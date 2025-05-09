70619614b5562e28c6b67d77a3be2e7d66481b5f
fauzanaslam <fauzanaslamdhani@gmail.com>
2025-05-09 08:42:31 +0700

    feat: store cart to localeStorage

--- apps/car-markt-web/src/app/stores/cart.store.ts
+++ apps/car-markt-web/src/app/stores/cart.store.ts
@@ -3,6 +3,8 @@ import { patchState, signalStore, withComputed, withMethods, withState} from "@n
 import { Product } from "../../../../car-markt-be/src/generated/prisma"
 import { computed } from "@angular/core";
 
+const CART_LOCALSTORAGE_KEY = 'car_markt_cart';
+
 type cartItem = Product & {
     quantity: number;
 }
@@ -19,7 +21,15 @@ export const CartStore = signalStore(
     {
         providedIn: 'root',
     },
-    withState(() => initialState),
+    withState(() => {
+        if ('localStorage' in globalThis) {
+            return {
+                ...initialState,
+                items: JSON.parse(localStorage.getItem(CART_LOCALSTORAGE_KEY) ?? '[]') as cartItem[],
+            }
+        }
+        return initialState;
+    }),
     withComputed((store) => ({
         totalItems: computed(() =>
             store.items().reduce((acc, item) => {
@@ -58,6 +68,8 @@ export const CartStore = signalStore(
                     }]
                 })
             }
+
+            localStorage.setItem(CART_LOCALSTORAGE_KEY, JSON.stringify(store.items()));
         },
         updateQuantity(productId: string, quantity: number) {
             const updatedItems = store
@@ -66,6 +78,7 @@ export const CartStore = signalStore(
             patchState(store, {
                 items: updatedItems,
             });
+            localStorage.setItem(CART_LOCALSTORAGE_KEY, JSON.stringify(updatedItems));
         },
         removeFromCart(productId: string) {
             const updatedItems = store
@@ -74,11 +87,13 @@ export const CartStore = signalStore(
             patchState(store, {
                 items: updatedItems,
             });
+            localStorage.setItem(CART_LOCALSTORAGE_KEY, JSON.stringify(updatedItems));
         },
         clearCart() {
             patchState(store, {
                 items: [],
             });
+            localStorage.removeItem(CART_LOCALSTORAGE_KEY);
         }
     }))
 )
